726bf54c6d6a05c3845bbede60a54c1042f77fbc
MANUELARUBIO <manuelarubioatehortua@iesantaelena.edu.co>
2023-04-19 16:24:39 -0500

    modificacion

--- script/ejercicio3.js
+++ script/ejercicio3.js
@@ -1,5 +1,20 @@
-let nombre, salarioB, deducciones, bonificaciont1, bonificaciont2
+let nombre, salario, deducciones, bonificaciones, nombre2, salario2;
 
-//
+//asignar informacion
 
-nombre=prompt('')
+nombre=prompt('ingrese nombre del trabajador 1')
+nombre2=prompt('ingrese nombre del trabajador 2')
+salario=Number(prompt('ingrese el salario de empleado 1'))
+salario2=Number(prompt('ingrese el salariode empleado 2'))
+deducciones=Number(prompt('ingrese las deducciones del trabajador'))
+
+//proceso
+
+if(salario>salario2){
+    alert()
+}
+
+
+
+
+
